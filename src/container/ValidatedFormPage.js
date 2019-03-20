1691fd0bb9b982d1fdc8e553596d4f108c8fd387
WHITING, JEREMY <jeremywhiting@northwesternmutual.com>
2019-03-20 12:21:56 -0500

    feat: initial commit

---
+++ src/container/ValidatedFormPage.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react"
+
+class ValidatedFormPage extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <h1>Validated Form</h1>
+      </div>
+    )
+  }
+}
+
+export default ValidatedFormPage
